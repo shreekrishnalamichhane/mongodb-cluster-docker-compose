a7cf866e74649281de2c468c98115c808e0e55da
shreekrishnalamichhane <uniksk123@gmail.com>
2023-07-02 16:12:38 +0545

    bug fix

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 # Step 1: Start all of the containers
 docker compose up -d
 
+sleep 10 # seconds
+
 # Step 2: Initialize the replica sets (config servers and shards)
 docker compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
+
 docker compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
 docker compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
 docker compose exec shard03-a sh -c "mongosh < /scripts/init-shard03.js"
 
 # Note: Wait a bit for the config server and shards to elect their primaries before initializing the router
-SLEEP 10
+sleep 20 # seconds
 
 # Step 3: Initializing the router
 docker compose exec router01 sh -c "mongosh < /scripts/init-router.js"
+
+sleep 10 # seconds
 
 # Step 4: Verify status of replica set for each shard
 docker exec -it shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017" 
